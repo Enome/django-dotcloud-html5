d4ed022dcc9c34071d8e9e469cd5ad36839075bb
Geert Pasteels <geert.pasteels@gmail.com>
2011-04-30 00:31:03 +0200

    fixed static root

--- django_project/settings.py
+++ django_project/settings.py
@@ -71,6 +71,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(SITE_ROOT, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -119,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_assets',
+    'south',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- django_project/settings_deployment.py
+++ django_project/settings_deployment.py
@@ -1,2 +1,4 @@
 #Deployment settings
 from settings import *
+
+STATIC_ROOT = os.path.join(SITE_ROOT,'../static')
