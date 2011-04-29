a9f38e18a924d4d5045967470078f3e4ee97a7ee
Geert Pasteels <geert.pasteels@gmail.com>
2011-04-29 22:19:33 +0200

    init ;

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+#Dotcloud only adds current to the python path
+import sys
+sys.path.append('django_project')
+
+#This is needed to get into the right django 'context'
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bookido.settings_deployment'
+
+import django.core.handlers.wsgi
+djangoapplication = django.core.handlers.wsgi.WSGIHandler()
+def application(environ, start_response):
+    if 'SCRIPT_NAME' in environ:
+        del environ['SCRIPT_NAME']
+    return djangoapplication(environ, start_response)
