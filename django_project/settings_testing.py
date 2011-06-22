d3c9dbb3db75ab7f6692226d7df2194be7a44fad
Geert Pasteels <grandmazbox@hotmail.com>
2011-06-22 04:18:55 -0700

    removed freshen form test runner

--- django_project/settings_testing.py
+++ django_project/settings_testing.py
@@ -10,4 +10,4 @@ DATABASES = {
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 #Includes support for freshen and bdd style tests for unit tests
-NOSE_ARGS = [r'-m', r'((?:^|[b_.-])(:?[Tt]est|When|should))', '--with-freshen',] 
+NOSE_ARGS = [r'-m', r'((?:^|[b_.-])(:?[Tt]est|When|should))', ]
